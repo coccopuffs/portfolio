d0b4b929fc82242380286200c7dcacc0a02a184a
coccopuffs <MK.Challapalli@gmail.com>
2022-05-08 10:01:40 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { createTheme, ThemeProvider, makeStyles } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2e1667",
+    },
+    secondary: {
+      main: "#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto"],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: "2rem",
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: "2rem",
+    },
+  },
+});
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}></ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
